ab44f0150ebba4afa0da4ddb2684dbdc3a1cd1ed
Mark Drummond <mjamesd@gmail.com>
2022-02-08 00:31:17 -0800

    Initial load respects fallback  - [x] IndexDB > localStorage > header

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,11 +4,9 @@ const initdb = async () =>
     openDB('jate', 1, {
         upgrade(db) {
             if (db.objectStoreNames.contains('jate')) {
-                // console.log('jate database already exists');
                 return;
             }
             db.createObjectStore('content', { keyPath: 'id', autoIncrement: true });
-            // console.log('jate database created');
         },
     });
 
@@ -17,8 +15,7 @@ export const putDb = async (content) => {
     const jateDb = await openDB('jate', 1);
     const tx = jateDb.transaction('content', 'readwrite');
     const store = tx.objectStore('content');
-    const result = await store.put({id: 1, content: content});
-    // console.log('JATE data saved to the database', result);
+    await store.put({id: 1, content: content});
 };
 
 // Method that gets all the content from the database
@@ -27,7 +24,6 @@ export const getDb = async () => {
     const tx = jateDb.transaction('content', 'readonly');
     const store = tx.objectStore('content');
     const result = await store.get(1);
-    // console.log('result.value', result);
     return result;
 };
 
